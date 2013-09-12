561e1dbe363736419a1b87cfe687520543224f7d
Jordan Santell <jsantell@gmail.com>
2013-09-12 20:53:13 +0200

    Rename collection

--- app/collections/components.js
+++
@@ -1,9 +0,0 @@
-var Backbone = require('backbone');
-var config = require('../config');
-
-module.exports = Backbone.Collection.extend({
-  model: require('../models/item'),
-  url: config.apiURL + 'items',
-  initialize: function () {
-  }
-});
